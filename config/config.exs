8530535474b796ff113b9ad98cea32158beedff1
Olga Draber <olgadraber@gmail.com>
2019-04-05 18:47:59 +0200

    distillery setup

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,5 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 use Mix.Config
 
-# Configures the endpoint
 config :game_of_life, GameOfLifeWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "Z9nTGzcv94Xigr4mclk2bugUzdfOTsvBVsFx8zczbFVPcV9hIh59cLgyE7oEldbX",
@@ -15,14 +7,10 @@ config :game_of_life, GameOfLifeWeb.Endpoint,
   pubsub: [name: GameOfLife.PubSub, adapter: Phoenix.PubSub.PG2],
   live_view: [signing_salt: "ixz/VHENhl4TOIw6NMmnvX5qwdvH2p4K"]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+if File.exists?("config/#{Mix.env()}.exs"), do: import_config("#{Mix.env()}.exs")
--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule GameOfLife.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:distillery, "~> 2.0"}
     ]
   end
 end
